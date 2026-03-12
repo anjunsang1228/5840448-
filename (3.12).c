a799394960c88b0ee9e0eed4fed0aa5696ea1868
anjunsang1228 <ananjun123@naver.com>
2026-03-12 14:14:03 +0900

    Update (3.12).c

--- (3.12).c
+++ (3.12).c
@@ -10,13 +10,14 @@ int main() {
     printf("정수 -1의 메모리 값 (16진수): 0x%02X\n", *ptr);
 
     if (*ptr == 0xFF) 
-        printf("결과: 2의 보수 방식을 사용하고 있습니다.\n");
+        printf("결과: 2의 보수입니다. \n");
     
     else if (*ptr == 0xFE) 
-        printf("결과: 1의 보수 방식을 사용하고 있습니다.\n");
+        printf("결과: 1의 보수입니다. \n");
     
 
     return 0;
 
 }
+
 
