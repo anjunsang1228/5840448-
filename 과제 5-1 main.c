a4ef656c3bc681fe98b8b156d8d000c39ef9efde
anjunsang1228 <ananjun123@naver.com>
2026-04-02 16:15:36 +0900

    Add files via upload

---
+++ 과제 5-1 main.c
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+
+int main(void) {
+    int arr[N];
+    int i;
+    int sum = 0;
+    int max, min;
+
+    srand((unsigned int)time(NULL));
+
+    for (i = 0; i < N; i++) {
+        arr[i] = rand() % (N + 1);
+    }
+
+    printf("배열을 처음부터 끝까지 출력:\n");
+    for (i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+
+    printf("배열을 뒤에서부터 처음까지 출력:\n");
+    for (i = N - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+
+    max = arr[0];
+    min = arr[0];
+
+    for (i = 0; i < N; i++) {
+        sum += arr[i];
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("배열 전체의 총합: %d\n", sum);
+    printf("배열의 최대값: %d\n", max);
+    printf("배열의 최소값: %d\n", min);
+
+    return 0;
+}
---
+++ 과제 5-2 main.c
@@ -0,0 +1,95 @@
+﻿#include <stdio.h>
+
+#define N 3
+
+int main(void) {
+    int magic[N][N];
+    int i, j;
+    int num = 1;
+    int row = 0;
+    int col = N / 2;
+    int newRow, newCol;
+    int sum;
+
+    if (N % 2 == 0) {
+        printf("N은 홀수여야 합니다.\n");
+        return 1;
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            magic[i][j] = 0;
+        }
+    }
+
+    while (num <= N * N) {
+        magic[row][col] = num;
+
+        newRow = row - 1;
+        newCol = col + 1;
+
+        if (newRow < 0) {
+            newRow = N - 1;
+        }
+
+        if (newCol >= N) {
+            newCol = 0;
+        }
+
+        if (magic[newRow][newCol] != 0) {
+            row = row + 1;
+            if (row >= N) {
+                row = 0;
+            }
+        }
+        else {
+            row = newRow;
+            col = newCol;
+        }
+
+        num++;
+    }
+
+    printf("홀수 마방진 (%d x %d)\n\n", N, N);
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%4d", magic[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n가로 합\n");
+    for (i = 0; i < N; i++) {
+        sum = 0;
+        for (j = 0; j < N; j++) {
+            sum += magic[i][j];
+        }
+        printf("%d행 합 = %d\n", i + 1, sum);
+    }
+
+    printf("\n세로 합\n");
+    for (j = 0; j < N; j++) {
+        sum = 0;
+        for (i = 0; i < N; i++) {
+            sum += magic[i][j];
+        }
+        printf("%d열 합 = %d\n", j + 1, sum);
+    }
+
+    printf("\n대각선 합\n");
+
+    sum = 0;
+    for (i = 0; i < N; i++) {
+        sum += magic[i][i];
+    }
+    printf("왼쪽 위 -> 오른쪽 아래 대각선 합 = %d\n", sum);
+
+    sum = 0;
+    for (i = 0; i < N; i++) {
+        sum += magic[i][N - 1 - i];
+    }
+    printf("오른쪽 위 -> 왼쪽 아래 대각선 합 = %d\n", sum);
+
+    return 0;
+}
